e623395a1a722bde1aba0031ed7b89432b52a5ef
masonomara <mason.omara@gmail.com>
2024-02-29 17:44:05 -0500

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,94 +1,67 @@
-import Image from "next/image";
 import styles from "./page.module.css";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+      <video
+        src={"opnsrc.mp4"}
+        autoPlay
+        playsInline
+        muted
+        loop
+        controls={false}
+        className={styles.video}
+      />
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+      <div className={styles.content}>
+        <div className={styles.titleWrapper}>
+          <div className={styles.logo}>
+            <Image src="/oss.png" alt="OSS" fill />
+          </div>
+          <h1 className={styles.logoTitle}>
+            OpenSource
+            <br />
+            Solutions
+          </h1>
+          <p className={styles.subtitle}>
+            Innovative software
+            <br />
+            solutions available for
+            <br />
+            work March 2024.
           </p>
-        </a>
+          <div className={styles.desktopTitle}>
+            <h1 className={styles.logoTitle}>OpenSource Solutions</h1>
+            <p className={styles.subtitle}>
+              Innovative software solutions available for work March 2024.
+            </p>
+          </div>
+        </div>
+        <div className={styles.infoWrapper}>
+          <div className={styles.infoContainer}>
+            <p className={styles.infoTitle}>Contact</p>
+            <div className={styles.infoLink}>Email Us &gt;</div>
+            <p className={styles.infoSub}>
+              Portfolio available
+              <br />
+              by request.
+            </p>
+          </div>
+          <div className={styles.infoContainer}>
+            <p className={styles.infoTitle}>Focuses</p>
+
+            <p className={styles.infoMain}>
+              Software Development
+              <br />
+              User Experience Design
+              <br />
+              Mobile and Web Apps
+              <br />
+              E-Commerce Solutions
+            </p>
+          </div>
+        </div>
       </div>
     </main>
   );
