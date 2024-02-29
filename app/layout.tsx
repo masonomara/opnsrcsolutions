c8c21e9687c79d79b0a619d6689e1f83c116d9dc
masonomara <mason.omara@gmail.com>
2024-02-29 17:59:43 -0500

    added anayltics and edited metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,34 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: {
-    default: "OpenSource Solutions",
-    template: "OpenSource Solutions | %s",
-  },
-  icons: {
-    icon: "icon.ico",
-  },
-  description:
-    "Software solutions company available for work March 2024. Scope includes design and development of apps, websites, e-commerce.",
-  openGraph: {
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: {
+      default: "OpenSource Solutions",
+      template: "OpenSource Solutions | %s",
+    },
+    metadataBase: new URL("https://opnsrcsolutions.com/"),
+    icons: {
+      icon: "icon.ico",
+    },
     description:
       "Software solutions company available for work March 2024. Scope includes design and development of apps, websites, e-commerce.",
-    images: [
-      {
-        url: "/social.png",
-      },
-    ],
-  },
-};
+    openGraph: {
+      description:
+        "Software solutions company available for work March 2024. Scope includes design and development of apps, websites, e-commerce.",
+      images: [
+        {
+          url: "/social.png",
+        },
+      ],
+    },
+  };
+}
 
 export default function RootLayout({
   children,
@@ -33,6 +38,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <Analytics />
     </html>
   );
 }
